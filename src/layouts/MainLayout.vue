9512796d5a77f337a935fa766ed8f66664daddd9
fallengravity <ethanvb55@gmail.com>
2021-02-19 23:00:50 +0200

    Remove unused libs

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -14,19 +14,6 @@
         </q-toolbar-title>
 
         <div>
-          <MetamaskChecker>
-            <div id="err" slot="errored" slot-scope="{ error }">
-              {{ error.message || 'Unexpected error' }}
-            </div>
-
-            <div id="app" slot="checked" slot-scope="{ provider, selectedAccount, selectedNetwork }">
-              Active Account: {{ selectedAccount }} On Network ID: {{ selectedNetwork }}
-            </div>
-
-            <div id="loader">
-              Loading Data
-            </div>
-          </MetamaskChecker>
         </div>
       </q-toolbar>
     </q-header>
@@ -86,13 +73,9 @@
 </template>
 
 <script>
-import MetamaskChecker from '@metamask-checker/vue';
 
 export default {
   name: 'MainLayout',
-  components: {
-    MetamaskChecker,
-  },
   data() {
     return {
       drawer: false,
