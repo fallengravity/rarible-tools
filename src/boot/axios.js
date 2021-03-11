39b734903b1c0fa2b9fa702403d82731bdad825c
fallengravity <ethanvb55@gmail.com>
2021-03-11 22:43:24 +0200

    Remove Wallets Not Used on Rarible + Publish New Build

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,51 +7,17 @@ const API = {};
 const apiKey = '50857081-537f-45cc-8ac2-589bddd619e3';
 const networkId = 1;
 const FORTMATIC_KEY = 'pk_live_37D874D6AE5A87C3';
-const PORTIS_KEY = '298edcdf-0694-46fa-87df-a8bc3b2de37a';
 const INFURA_KEY = 'ad2ee80801ce45de9dd717e612c904cb';
-const APP_URL = 'https://tools.gravityenterprises.co.za';
-const CONTACT_EMAIL = 'ethan@rarible.com';
 const RPC_URL = 'https://mainnet.infura.io/v3/4ece25e8c99949f4ba77d08942399082';
 const APP_NAME = 'Rarible Tools';
 const wallets = [
-  {
-    walletName: 'coinbase',
-  },
-  {
-    walletName: 'trust',
-    rpcUrl: RPC_URL,
-  },
   {
     walletName: 'metamask',
     preferred: true,
   },
   {
-    walletName: 'authereum',
-  },
-  {
-    walletName: 'trezor',
-    appUrl: APP_URL,
-    email: CONTACT_EMAIL,
-    rpcUrl: RPC_URL,
-  },
-  {
-    walletName: 'ledger',
-    rpcUrl: RPC_URL,
-    preferred: true,
-  },
-  {
-    walletName: 'lattice',
-    rpcUrl: RPC_URL,
-    appName: APP_NAME,
-  },
-  {
     walletName: 'fortmatic',
     apiKey: FORTMATIC_KEY,
-    preferred: true,
-  },
-  {
-    walletName: 'portis',
-    apiKey: PORTIS_KEY,
     preferred: true,
   },
   {
@@ -60,47 +26,10 @@ const wallets = [
     preferred: true,
   },
   {
-    walletName: 'opera',
-  },
-  {
-    walletName: 'operaTouch',
-  },
-  {
-    walletName: 'torus',
-  },
-  {
-    walletName: 'status',
-  },
-  {
     walletName: 'walletLink',
     rpcUrl: RPC_URL,
     appName: APP_NAME,
     preferred: true,
-  },
-  {
-    walletName: 'imToken',
-    rpcUrl: RPC_URL,
-  },
-  {
-    walletName: 'meetone',
-  },
-  {
-    walletName: 'mykey',
-    rpcUrl: RPC_URL,
-  },
-  {
-    walletName: 'huobiwallet',
-    rpcUrl: RPC_URL,
-  },
-  {
-    walletName: 'hyperpay',
-  },
-  {
-    walletName: 'wallet.io',
-    rpcUrl: RPC_URL,
-  },
-  {
-    walletName: 'atoken',
   },
 ];
 
